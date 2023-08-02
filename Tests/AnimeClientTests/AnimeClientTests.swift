930961b7e23741ec8363039c0977ca5cdcd38e1f
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2023-08-02 17:26:31 -0500

    fix(animeclient): remove tests

--- Tests/AnimeClientTests/AnimeClientTests.swift
+++ Tests/AnimeClientTests/AnimeClientTests.swift
@@ -2,12 +2,4 @@ import XCTest
 
 @testable import AnimeClient
 
-final class AnimeClientTests: XCTestCase {
-    func testFetchingAllEpisodeProviders() async throws {
-        let animeClient = AnimeClient.testValue
-
-        let providers = try await animeClient.getAnimeProviders()
-
-        XCTAssertEqual(providers.count, 3)
-    }
-}
+final class AnimeClientTests: XCTestCase {}
